c2ae48b85210b9ae9b21bd85f499bf067cb00e27
Inbeom Moon <dlsqja567@naver.com>
2024-05-19 15:19:14 +0900

    [Setting] #1 - 프로젝트 기본 세팅

---
+++ Halmooni/Views/MainTabView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Halmooni
+//
+//  Created by 문인범 on 5/19/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab: Int = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            // TODO: - 목표관리 View 넣어주세요
+            Text("목표관리뷰")
+                .tabItem {
+                    // 수정 예정
+                    Image(systemName: "scope")
+                    Text("목표 관리")
+                }
+            
+            // TODO: - 모아보기 View 넣어주세요
+            Text("모아보기뷰")
+                .tabItem {
+                    Image(systemName: "square.grid.2x2.fill")
+                    Text("모아 보기")
+                }
+        }
+        .tint(Color("PrimColor"))
+    }
+}
+
+#Preview {
+    MainTabView()
+}
