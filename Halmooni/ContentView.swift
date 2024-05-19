6d312a11baa24b9e3edb2b934eb5581bdc904485
Inbeom Moon <dlsqja567@naver.com>
2024-05-19 14:42:42 +0900

    Initial Commit

---
+++ Halmooni/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Halmooni
+//
+//  Created by 문인범 on 5/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Halmooni/HalmooniApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalmooniApp.swift
+//  Halmooni
+//
+//  Created by 문인범 on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HalmooniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
