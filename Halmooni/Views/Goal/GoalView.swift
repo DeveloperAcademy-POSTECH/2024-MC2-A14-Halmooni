0da9b6b9ce223fc83f7e067c5eff1f5c24d80d99
SeoyeonChoo <sy87.business@gmail.com>
2024-05-20 15:43:44 +0900

    [Feature] /#3 - view 수정

---
+++ Halmooni/Views/Goal/GoalView.swift
@@ -0,0 +1,38 @@
+//
+//  GoalView.swift
+//  Halmooni
+//
+//  Created by 추서연 on 5/19/24.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    @State private var tokenSum = 15
+    @State private var tokenUsed = 8
+    
+    var body: some View {
+        ZStack {
+            Color("BgColor").edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("목표 관리")
+                    .padding(16)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                HStack {
+                    CurrentStamp(tokenSum: tokenSum, tokenUsed: tokenUsed)
+                    Spacer()
+                    AddStamp(tokenSum: tokenSum, tokenUsed: tokenUsed)
+                }
+                GaugeBar()
+                CalendarView(month: Date())
+            }
+            .padding(16)
+        }
+    }
+}
+
+#Preview {
+    GoalView()
+}
