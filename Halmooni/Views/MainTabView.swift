17d5cc3279df26103d179fcc423f09cc33ba8f1e
Guryss <lala1470@konkuk.ac.kr>
2024-05-21 15:07:36 +0900

    [Fix] #7 - MainTabView 내 navigationTitle과 enum 연결 방식 수정

--- Halmooni/Views/MainTabView.swift
+++ Halmooni/Views/MainTabView.swift
@@ -8,28 +8,28 @@
 import SwiftUI
 
 struct MainTabView: View {
-    @State private var selectedTab: Int = 0
+    @State private var selectedTab: Title = .goal
 
     var body: some View {
         NavigationStack {
             TabView(selection: $selectedTab) {
                 GoalView()
                     .tabItem {
-                        // 수정 예정
-                        Image(systemName: "scope")
+                        Image(systemName: "trophy.fill")
                         Text(Title.goal.name)
                     }
-                    .tag(0)
+                    .tag(Title.goal)
+                
                 // TODO: - 모아보기 View 넣어주세요
                 Text("모아보기뷰")
                     .tabItem {
                         Image(systemName: "square.grid.2x2.fill")
                         Text(Title.list.name)
                     }
-                    .tag(1)
+                    .tag(Title.list)
             }
             .tint(Color(.prim))
-            .navigationTitle(selectedTab == 0 ? Title.goal.name : Title.list.name)
+            .navigationTitle(selectedTab.name)
         }
     }
 }
