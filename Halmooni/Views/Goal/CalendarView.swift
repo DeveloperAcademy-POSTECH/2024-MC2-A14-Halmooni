74b8d41dfb3b59da2b3fc54bb461ec100b7c912c
SeoyeonChoo <sy87.business@gmail.com>
2024-05-24 22:44:10 +0900

    [Design]/#96 스플래시 뷰 모드 선택 뷰 수정

--- Halmooni/Views/Goal/CalendarView.swift
+++ Halmooni/Views/Goal/CalendarView.swift
@@ -20,6 +20,7 @@ struct CalendarView: View {
         .padding(16)
         .background(.section)
         .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(color: .gry2, radius: 10, x: 0, y: 5)
         
         .gesture(
             DragGesture()
@@ -137,6 +138,8 @@ private struct CellView: View {
                 .overlay(Text(String(day)))
                 .frame(width: 33, height: 33)
                 .background(isToday ? Color.sec : Color.clear)
+                .font(isToday ? .title2 : .subheadline)
+                .cornerRadius(20)
                 .overlay(
                     Group {
                         if selected {
@@ -144,10 +147,9 @@ private struct CellView: View {
                                 .resizable()
                                 .frame(width: 23, height: 23)
                                 .foregroundColor(Color.white)
-                            Image(systemName: "star.circle.fill")
+                            Image(systemName: "heart.circle.fill")
                                 .resizable()
                                 .frame(width: 23, height: 23)
-                                .rotationEffect(.degrees(30))
                                 .foregroundColor(Color.prim)
                         }
                     }
